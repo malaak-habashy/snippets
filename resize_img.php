199ac77ee4d34d52f836bc395319bdcf33744a1c
Malaak Habashy <malaak.habashy@gmail.com>
2021-05-22 12:56:30 +0200

    init

---
+++ resize_img.php
@@ -0,0 +1,57 @@
+if (!empty($_FILES)) {
+    $tempFile = $_FILES['file']['tmp_name'];
+    $fileSize = getimagesize($tempFile);
+    $width = $fileSize[0];
+    $height = $fileSize[1];
+
+   
+   
+    $targetFile =  $targetPath . $newfilename;
+ 
+    $uploaded = false;
+    if ($width >= 2000 || $height >= 2000) {
+      
+        $uploaded = resize(2000,$targetFile, $tempFile);
+}
+
+
+function resize($newWidth, $targetFile, $originalFile) {
+
+    $info = getimagesize($originalFile);
+    $mime = $info['mime'];
+
+    switch ($mime) {
+            case 'image/jpeg':
+                    $image_create_func = 'imagecreatefromjpeg';
+                    $image_save_func = 'imagejpeg';
+                    $new_image_ext = 'jpg';
+                    break;
+
+            case 'image/png':
+                    $image_create_func = 'imagecreatefrompng';
+                    $image_save_func = 'imagepng';
+                    $new_image_ext = 'png';
+                    break;
+
+            case 'image/gif':
+                    $image_create_func = 'imagecreatefromgif';
+                    $image_save_func = 'imagegif';
+                    $new_image_ext = 'gif';
+                    break;
+
+            default: 
+                    throw new Exception('Unknown image type.');
+    }
+
+    $img = $image_create_func($originalFile);
+    list($width, $height) = getimagesize($originalFile);
+
+    $newHeight = ($height / $width) * $newWidth;
+    $tmp = imagecreatetruecolor($newWidth, $newHeight);
+    imagecopyresampled($tmp, $img, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
+    if (file_exists($targetFile)) {
+            unlink($targetFile);
+    }
+    return $image_save_func($tmp, "$targetFile");
+}
